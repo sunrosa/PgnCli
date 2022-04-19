9e4f2005a84ccb014cfc1a2386a0dbbd906afb6e
Ibis Sunrosa <drsunrosa@gmail.com>
2022-04-19 16:06:49 -0500

    CommandLineParser test

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace PgnCli
+﻿using System;
+using CommandLine;
+
+namespace PgnCli
 {
+    public class Options
+    {
+        [Option('v', "verbose", Required = false, HelpText = "Verbose output")]
+        public bool Verbose {get; set;}
+    }
+
     public class Program
     {
         public static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world!");
+            Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            {
+                if (o.Verbose)
+                {
+                    Console.WriteLine("Verbose!!!");
+                }
+            });
         }
     }
 }
