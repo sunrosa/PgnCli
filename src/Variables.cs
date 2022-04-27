6f57c4ad6716b026a6ab3172f50662f4c0afe035
Ibis Sunrosa <drsunrosa@gmail.com>
2022-04-27 10:46:14 -0500

    Documentation of variables

--- src/Variables.cs
+++ src/Variables.cs
@@ -2,8 +2,29 @@ namespace PgnCli
 {
     public static class Variables
     {
+        /// <summary>
+        /// The name of this program.
+        /// </summary>
+        public static string ProgramName = "pgncli";
+
+        /// <summary>
+        /// Version of this program.
+        /// </summary>
+        public static string Version = "0.0.0.0";
+
+        /// <summary>
+        /// Path to the root of this program.
+        /// </summary>
         public static string ProgramPath = "C:\\Users\\itsyu\\Desktop\\git\\pgncli";
+
+        /// <summary>
+        /// Path to local python executable.
+        /// </summary>
         public static string PythonPath = "C:\\Program Files\\Python39\\python.exe";
+
+        /// <summary>
+        /// Minimum rating deviation required to show a question mark to the right of ratings presented in the UI.
+        /// </summary>
         public static double QuestionMarkMinimumDeviation = 150;
     }
 }
