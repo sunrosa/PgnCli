b4780e6a37eefc09694f81ed11fa6d59c76253ed
Ibis Sunrosa <drsunrosa@gmail.com>
2022-04-26 23:07:08 -0500

    Sorted JArray of games

--- src/Verbs.cs
+++ src/Verbs.cs
@@ -4,7 +4,9 @@ namespace PgnCli
     {
         public static void Glicko(GlickoOptions options)
         {
-            Console.WriteLine(Import.PgnToJson(System.IO.Path.GetFullPath(options.File)));
+            var unsortedPgn = Import.PgnToJson(System.IO.Path.GetFullPath(options.File)); // Import pgn into JArray from path
+            var pgn = new Newtonsoft.Json.Linq.JArray(unsortedPgn.OrderBy(obj => DateTime.ParseExact(obj["_tag_roster"]["Date"].ToObject<string>(), "yyyy.MM.dd", System.Globalization.CultureInfo.InvariantCulture))); // Sort pgn by game date
+            Console.WriteLine(pgn);
         }
     }
 }
